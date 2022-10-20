2b115890156280040f1ce4d2e2a017d450a59327
Lam <volam13062001@gmail.com>
2022-10-20 14:13:44 +0700

    Feat: Init Commit

---
+++ src/main/java/com/molla/MollaShopApplication.java
@@ -0,0 +1,13 @@
+package com.molla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MollaShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MollaShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/molla/MollaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.molla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MollaShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
